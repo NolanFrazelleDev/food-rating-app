a1c6a4c71632aecb68a0ac2ef594b9c24eaebc4a
Nolan Frazelle <frazellen@gmail.com>
2022-08-02 14:26:20 -0700

    setup food rating app

---
+++ README.m
@@ -0,0 +1,4 @@
+# food-rating-app
+
+Hello! This is a Food Rating App where it helps me decide what I want to eat today. To start this app will show a leaderboard of food that I like. Future ideas include to make different leaderboards for different meal types and to search for parameters.  
+
